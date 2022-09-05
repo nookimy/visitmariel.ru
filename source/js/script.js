bebc3a48ad3e882bfa62d0ae0897c56943b43854
Alina <alina.onmaster@gmail.com>
2022-09-05 14:38:39 +0300

    Черновик меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
 let menuButton = document.querySelector(".burger-button");
+let nav = document.querySelector(".site-nav");
 
 menuButton.onclick = function () {
   menuButton.classList.toggle("burger-button--opened");
+  nav.classList.toggle("site-nav--opened");
 };
