011c8ac482af17b21dde6a69ddc8c721b1285f24
Alina <alina.onmaster@gmail.com>
2022-09-05 12:53:23 +0300

    Кнопка бургер

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,5 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+let menuButton = document.querySelector(".burger-button");
 
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
+menuButton.onclick = function () {
+  menuButton.classList.toggle("burger-button--opened");
+};
