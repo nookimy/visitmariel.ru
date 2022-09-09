3e8f16b05dafed5241f8e5893c2506543bfa726a
Alina <alina.onmaster@gmail.com>
2022-09-09 15:53:16 +0300

    Блок Спецпроекты

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,8 @@
 let menuButton = document.querySelector(".burger-button");
 let nav = document.querySelector(".site-nav");
 
-menuButton.onclick = function () {
+/*menuButton.onclick = function () {
   menuButton.classList.toggle("burger-button--opened");
   nav.classList.toggle("site-nav--opened");
-};
+};*/
+
--- source/js/swiper-slider.js
+++ source/js/swiper-slider.js
@@ -1,7 +1,61 @@
-new Swiper('.swiper',{
+let windowWidth = document.documentElement.clientWidth;
+
+new Swiper('.main-slider.swiper',{
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
   slidesPerView: 1,
 });
+
+
+
+let projectsSwiper;
+let projectsSwiperBlock = document.querySelector(".projects__swiper");
+var projectsList = document.querySelector('.projects__list');
+var projectsItem = document.querySelectorAll('.projects__item');
+
+function removeClassesSwiper() {
+  if (windowWidth < 768) {
+    projectsSwiperBlock.classList.remove("swiper");
+    projectsList.classList.remove("swiper-wrapper");
+    projectsItem.classList.remove("swiper-slide");
+  }
+};
+
+removeClassesSwiper();
+
+function checkScreen() {
+  if (windowWidth >= 768) {
+    projectsSwiper = new Swiper('.projects__swiper.swiper', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 35,
+        },
+
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 35,
+        },
+      },
+
+    });
+  } else {
+    projectsSwiper.destroy(); // или swiper.autoplay.stop();
+  }
+};
+
+checkScreen();
+
+
